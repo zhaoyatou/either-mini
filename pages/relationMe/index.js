1043c66407344f6c686163681011ac37ebcf7982
zz080315 <zhaoan>
2025-02-12 15:10:58 +0800

    bug修复

--- pages/relationMe/index.js
+++ pages/relationMe/index.js
@@ -16,6 +16,12 @@ Page({
       scanType: ['qrCode'],
     })
   },
+  previewImage() {
+    wx.previewImage({
+      current: 'https://cdn.eithergo.com/image/2025-02-11/2_w_1062_h_2366_1739255896_b6a3.jpg', // 当前显示图片的链接
+      urls: ['https://cdn.eithergo.com/image/2025-02-11/2_w_1062_h_2366_1739255896_b6a3.jpg', 'https://cdn.eithergo.com/image/2025-02-11/2_w_1058_h_2334_1739255924_1c19.jpg'] // 需要预览的图片链接列表（可添加多个URL以实现多张图片预览）
+    });
+  },
   call() {
     wx.makePhoneCall({
       phoneNumber: '17812061710'
